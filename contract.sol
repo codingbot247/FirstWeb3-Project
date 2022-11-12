44371d523c990d5f8046ba084fe287cf7ff7caab
codingbot <58110004+codingbot247@users.noreply.github.com>
2022-11-13 04:24:05 +0530

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.1;
+
+contract MoodDiary{
+    
+    //creating a variable called mood
+    string mood;
+    
+    //creating a function that writes a mood to the smart contract
+    function setMood(string memory _mood) public{
+        mood = _mood;
+    }
+    
+    //creating a function the reads the mood from the smart contract
+    function getMood() public view returns(string memory){
+        return mood;
+    }
+}
