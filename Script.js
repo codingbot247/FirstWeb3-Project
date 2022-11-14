f2304ac83758a0d53ad8442f1875b00fedaf8e15
codingbot <58110004+codingbot247@users.noreply.github.com>
2022-11-15 02:18:04 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,49 @@
+const MoodContractAddress = "0x65B1174cDBb463e0F9132c09aaEDc63b02986D7D";
+  const MoodContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+  let MoodContract;
+  let signer;
+  provider.listAccount().then(function(accounts) {
+    signer = provider.getsigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+        MoodContractAddress, MoodContractABI, signer
+    );
+  });
 
+  async function getMood(){
+      getMoodPromise = MoodContract.getMood();
+      var Mood= await getMoodPromise;
+      console.log(Mood);
+  }
+
+  async function setMood(){
+      let mood= getElementbyid("mood").value;
+      setMoodPromise = MoodContract.setMood(mood);
+      await setMoodPromise;
+  }
